fd46c5884e8d931ba25c62432a087135fefde4f7
onlyno999 <105115958+onlyno999@users.noreply.github.com>
2025-06-24 08:17:58 +0800

    Update src.js

--- src.js
+++ src.js
@@ -11,10 +11,10 @@ let 优选列表 = [];
 let SOCKS5代理 = false;
 let SOCKS5全局代理 = false;
 
-let 反代IP = "ts.hpc.tw";
+let 反代IP = "wok.woxxxxxx.nyc.mn";
 
 let NAT64前缀 = "2001:67c:2960:6464::";
-let DOH地址 = "1.1.1.1";
+let DOH地址 = "114.114.114.114";
 
 // 网页入口
 export default {
